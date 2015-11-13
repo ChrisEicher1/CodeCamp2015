66d0d14ad756fe8cc1d712cd6c436d143c65e7d6
Chris Eicher <chris@eicher.com>
2015-11-13 15:46:59 -0700

    newgame/updategif funcs

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,35 @@
 console.log('Script has loaded');
 
-var socket = io();
 
+var updateGif = function(req)
+{
+	if ($("#" + req.id).length){
+		$("#"+req.id).attr('src', req.url);
+	} else {
+		console.log("You dun.");
+		var newImg = $('<img id="'+req.id+'" src="'+req.url+'">');
+		console.log(newImg);
+		newImg.appendTo('#profileDiv');
+	}
+}
+
+var newGame = function()
+{
+	$('#content').empty();
+	var div = $('<div class="row full"></div>')
+  	var profileDiv = $('<div id="profileDiv" class="col s3"></div>');
+  	var playDiv = $('<div class="col s9"></div>');
+  	div.appendTo('#content');
+  	profileDiv.appendTo(".row");
+}
+
+var socket = io();  
 socket.on('gameCreated', function(data){
-  Materialize.toast('I am a toast!', 4000);
-  var content = $('#content');
-  content.remove();
-  content.append('<image src="https://media.giphy.com/media/13zZ0FyrgNWwLu/giphy.gif" />')
+  newGame();
+  //updateGif({ id: '765', url: 'https://media.giphy.com/media/13zZ0FyrgNWwLu/giphy.gif' })
+
+
+  //updateGif({ id: '123', url: 'https://media.giphy.com/media/13NBiMh0Z7pqta/giphy.gif' })
 })
+
+socket.on('gif', updateGif);
