70e00cf288053891c1eae983b7330c76052ff0f7
ncooper4 <neal@shadowtopstudios.com>
2015-11-13 14:36:07 -0700

    added test for trends

---
+++ test/trendsApi.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let Promise = require('bluebird');
+let trendsApi = require('../lib/trendsApi.js');
+
+trendsApi.getTrends()
+.then(data () => {
+  console.log(data);
+},err () => {
+  throw err;
+});
