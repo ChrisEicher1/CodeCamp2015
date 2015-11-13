3f39c0370d05b157f7c7ec10d6bc2d2be0edfba1
Michael Sorenson <m.sorenson407@gmail.com>
2015-11-13 13:53:48 -0700

    Removed divs when game is joined.

--- static/js/app.js
+++ static/js/app.js
@@ -3,5 +3,8 @@ console.log('Script has loaded');
 var socket = io();
 
 socket.on('gameCreated', function(data){
-	 Materialize.toast('I am a toast!', 4000);
+  Materialize.toast('I am a toast!', 4000);
+  var content = $('#content');
+  content.remove();
+  content.append('<image src="https://media.giphy.com/media/13zZ0FyrgNWwLu/giphy.gif" />')
 })
