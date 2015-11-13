3cdd3368b64f39e595b40e9a75133391173259fa
ncooper4 <neal@shadowtopstudios.com>
2015-11-13 15:41:22 -0700

    added sending of gifs to players.

--- test/trendsApi.js
+++ test/trendsApi.js
@@ -3,9 +3,16 @@
 let Promise = require('bluebird');
 let trendsApi = require('../lib/trendsApi.js');
 
-trendsApi.getTrends()
+let count = process.argv[2];
+if(! count){
+  console.log('count is undefined! %s');
+  console.log(process.argv);
+  process.exit(1);
+}
+trendsApi.getTrends(count)
 .then(data => {
   console.log(data);
+  console.log(data.trends.length);
 },err => {
   throw err;
 });
